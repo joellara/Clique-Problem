85e609059b29d1f9b66db6584a8db2e2fe124c8d
Joel Lara <joel.lq@hotmail.com>
2016-05-11 22:30:33 -0500

    Inital commit

---
+++ app.py
@@ -0,0 +1,14 @@
+import os
+
+from flask import *
+
+UPLOAD_FOLDER = '/static/'
+
+app = Flask(__name__)
+
+@app.route("/", methods=['GET', 'POST'])
+def index():
+    return render_template('index.html')
+	
+if __name__ == "__main__":
+    app.run(host=os.getenv("IP", "0.0.0.0"),port=int(os.getenv("PORT", 8081)),debug=True)
